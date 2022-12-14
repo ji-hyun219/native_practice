a799078703a2c6637155a1caeffa76299bea8054
ji-hyun219 <ix219@hanmail.net>
2022-12-14 14:49:05 +0900

    MainActivity.kt 가이드라인

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 네이티브 개념 공부 및 연습 레포지토리
 
 ### AOS - BatteryManager, iOS - device.batteryLevel (getBatteryLevel)
+
+### Kotlin 을 사용해서 Android 플랫폼 구현 추가
+
+MainActivty.kt 파일 수정
--- android/app/src/main/kotlin/com/example/native_practice/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_practice/MainActivity.kt
@@ -3,4 +3,18 @@ package com.example.native_practice
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "samples.flutter.dev/battery"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // onCreate 메서드 안에서 MethodChannel 을 만들고 setMethodCallHandler 를 호출하세요
+    // Flutter 클라이언트 쪽에서 사용한 것과 같은 채널 이름을 사용했는지 확인해주세요.
+    super.onCreate(savedInstanceState)
+
+    GeneratedPluginRegistrant.registerWith(this)
+    MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+      // Note: this method is invoked on the main thread.
+      // TODO
+    }
+
+  }
 }
